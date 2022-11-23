9a695ee5e80f4fa0c1036510fccf4a9805cfabb2
Rita Sabri <rsabri@seas.upenn.edu>
2022-11-23 15:35:11 -0500

    sds
    sds

--- experian.java
+++ experian.java
@@ -1,7 +1,8 @@
 
-import java.*;
+ import java. util. *;
+ import java.lang.StringBuilder;
 
-
+/*
 //task1
 
 int Solution ( public int solution(int X, int Y, int[] A) ){
@@ -56,40 +57,62 @@ public int solution(String s) {
     }
 }
 
+}
+*/
+
 //task3
 
+ class Solution {
 
-public String solution (int [] stack1, int [] stack2 , int [] stack3) {
+ public static String solution(int [] stack1, int [] stack2 , int [] stack3) {
+
+    String s = "";
 
     List a1 = Arrays.asList(stack1);
     List a2 = Arrays.asList(stack2);
     List a3 = Arrays.asList(stack3);
 
-    int lastelement1 = stack1[stack1.length-1]; 
-    int lastelement2 = stack2[stack2.length-1];
-    int lastelement3=  stack3[stack3.length-1];
-    String string  = " ";
-    
-//53
-    // as long as one of them is not null;
-    //if last number  is the biggest then we append it to the string;
-    //and then we pop the element 
-    
-    while ( stack1  !=null  || stack2 !=null || stack3  !=null){
-    if ( lastelement1 > lastelement2 && lastelement1 > lastelement3 ){
-        stack1.remove(stack1.length-1);  
+    int last_element1 = a1.size()-1;
+    int last_element2 = a2.size()-1;
+    int last_element3 = a3.size()-1;
+
+    int maximum = Math.max(last_element1, Math.max(last_element2,last_element3));
+ 
+
+    if ( maximum == last_element1 ){
+        a1.remove(last_element1);
+        s="1-";
+
+    }
+    if ( maximum == last_element2 ){
+        a2.remove(last_element2);
+        s="2-";
+
+    }
+    if ( maximum == last_element3 ){
+        a3.remove(last_element1);
+        s="3-";
+
     }
 
-    if ( lastelement2 > lastelement1 && lastelement2 > lastelement3 ){
-        string.append(2);
-        stack2.remove(stack2.length-1);
+    return s;
+
     }
-    if ( lastelement3 > lastelement1 && lastelement3 > lastelemen2 ){
-        string.append(3);
-        stack3.remove(stack3.length-1);
+
+
+    public static void main(String[] args){
+        
+        int [] stack1= {2,7};
+        int [] stack2 = {4,5};
+        int [] stack3 = {1};
+
+        System.out.print(solution(stack1,stack2,stack3));
+
+
+
     }
+
+
+
 }
 
-    return string;
-
-}
